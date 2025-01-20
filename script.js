ed50880a38325081f2141610ced8d8a775dc73c6
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-20 17:11:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let age = document.getElementById("age").value;
+let name = document.getElementById("name").value;
+
+let promise1 = new Promise((resolve, reject)=>{
+	if(age > 18){
+		setTimeout(()=>{
+			resolve(`Welcome, ${name}. You can vote.`);
+		}, 4000);
+	} else {
+		reject(`Oh sorry ${name}. You aren't old enough.`);
+	}
+});
+
+promise1.then((data)=>{
+	alert(date)
+}).catch(err){
+	alert(err);
+}
